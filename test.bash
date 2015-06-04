e5cdc9d3b4eba5bf248b41abbcb2086c9bb5b012
Inoue <a1507ni@aiit.ac.jp>
2015-06-04 19:51:20 +0900

    Addition of a shellscript

--- test.bash
+++ test.bash
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-echo "This is a Test."
-echo "AAAAA"
+tmp=/tmp/$$
+
+ERROR_EXIT () {
+	echo "$1" >&2
+	rm -f $tmp-*
+	exit 1
+}
+
+
+echo aaa bbb NG > $tmp-ans
+./same.py aaa bbb > $tmp-out || ERROR_EXIT "TEST1-1"
+diff $tmp-ans $tmp-out || ERROR_EXIT "TEST1-2"
+
+echo OK rm -f $tmp-*
+exit 0
